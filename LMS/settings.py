de3a294a4b3803bb93966afbf136b250306a410c
Abdul12majid <yisaabdulmajid@gmail.com>
2024-07-04 10:02:09 +0100

    request

--- LMS/settings.py
+++ LMS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tovf385tj=er5=&5i1poio@292ldds1zn&-&vbt@^sv&fsmk3m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'libraria-6dgu.onrender.com']
 
